15734cb10e91ab1440707aa987aaf7a88dcd939e
Alejandro Gonzalez <alejandrojgt@gmail.com>
2017-10-03 14:36:38 +0900

    Switched to Python 3. Added setup.py. Deleted unnecessary files. Renamed resources folder as kerasdcgan.

--- kerasdcgan/etl.py
+++ kerasdcgan/etl.py
@@ -5,26 +5,29 @@ import numpy as np
 from PIL import Image
 
 
-def read_etl_file(f, sample_size, bits_per_pixel, image_shape, unpack_format, image_idx):
-    """Reads contents of an ETL file.
+def read_etl_file(f, sample_size, bits_per_pixel, image_shape, unpack_format, charcode_idx, image_idx):
+    """Reads a sample from an ETL file.
     Args:
         f (file): File iterator.
         sample_size (int): Size of sample in bytes.
         bits_per_pixel (int): Bits per pixel.
         image_shape (tuple): Shape of image sample (width-x, height-y).
         unpack_format (str): Format to unpack byte stream (see struct documentation for details).
+        charcode_idx (int): Index of component containing the character code of the image.
         image_idx (int): Index of component containing the image.
 
     Returns:
-        Sample data (list of tuples).
+        Data sample (list containing the information of a record).
     """
     bytestream = f.read(sample_size)
-    sample = struct.unpack(unpack_format, bytestream)
+    sample = list(struct.unpack(unpack_format, bytestream))
+
+    sample[charcode_idx] = sample[charcode_idx].decode('ascii')
 
     img = Image.frombytes('F', image_shape, sample[image_idx], 'bit', bits_per_pixel)
     img = img.convert('L')
     img = Image.eval(img, lambda x: (255.0 * x) / (2 ** bits_per_pixel - 1))  # rescale to 0~255 range
-    sample += (img,)
+    sample += [img]
 
     return sample
 
@@ -44,17 +47,18 @@ def read_etl6_data(basepath):
     bits_per_pixel = 4  # bits
     image_shape = (64, 63)  # pixels
     unpack_format = '>H2sH6BI4H4B2H2016s4x'
+    charcode_idx = 1
     image_idx = 20
 
     data = []
     for path in paths:
         n_samples = os.stat(path).st_size // sample_size
 
-        with open(path, 'r') as f:
+        with open(path, 'rb') as f:
             for i in range(n_samples):
                 f.seek(i * sample_size)
                 data.append(read_etl_file(f, sample_size, bits_per_pixel,
-                                          image_shape, unpack_format, image_idx))
+                                          image_shape, unpack_format, charcode_idx, image_idx))
 
     return data
 
